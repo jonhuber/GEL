fbf02bc0e6f17c45f4594b2403476dabf41b4c6b
Simon Vrachliotis <simon@simonswiss.com>
2019-10-29 08:07:10 +1100

    cleaning up alert component

--- website/site/pages/components/alert/alert.js
+++ website/site/pages/components/alert/alert.js
@@ -1,29 +1,24 @@
 /** @jsx jsx */
-import { useEffect, useState } from 'react';
 import { jsx, useBrand } from '@westpac/core';
-
 import { Alert } from '@westpac/alert';
-import ReactLive from '../../../components/react-live';
 
 import { BOM, warning, danger, system } from '../../../../../components/alert/website';
+import ReactLive from '../../../components/react-live';
 
+// TODO: scope doesn't seem to work
 const scope = { Alert };
-console.log({ scope });
-const TokensPage = () => {
-	const brand = useBrand();
 
-	return (
-		<div>
-			<ReactLive code={BOM} scope={scope} />
-			<hr />
-			<ReactLive code={warning} scope={scope} />
-			<hr />
-			<ReactLive code={danger} scope={scope} />
-			<hr />
-			<ReactLive code={system} scope={scope} />
-		</div>
-	);
-};
+const TokensPage = () => (
+	<div>
+		<ReactLive code={BOM} scope={scope} />
+		<Spacer />
+		<ReactLive code={warning} scope={scope} />
+		<Spacer />
+		<ReactLive code={danger} scope={scope} />
+		<Spacer />
+		<ReactLive code={system} scope={scope} />
+	</div>
+);
 
 const Spacer = () => <div css={{ height: 60 }}></div>;
 
